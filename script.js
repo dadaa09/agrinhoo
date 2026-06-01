332cdb0e5b9ac4d8d184bbe51cd6fc80b16d4003
dadaa09 <maria.rodrigues.adami@escola.pr.gov.br>
2026-06-01 11:19:42 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function mostrarInfo() {
+    const info = document.getElementById("extra");
+
+    if (info.style.display === "block") {
+        info.style.display = "none";
+    } else {
+        info.style.display = "block";
+    }
+}
